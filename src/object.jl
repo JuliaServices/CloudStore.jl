44a307acc4053580fc2cb466728694ec94676f6e
Jacob Quinn <quinn.jacobd@gmail.com>
2022-11-07 16:29:16 -0700

    Add prefetching of next chunk to IOObject readbytes!

--- src/object.jl
+++ src/object.jl
@@ -35,22 +35,35 @@ function Base.copyto!(dest::AbstractVector{UInt8}, doff::Integer, src::Object, s
     return unsafe_copyto!(dest, doff, src, soff, n)
 end
 
-function Base.unsafe_copyto!(dest::AbstractVector{UInt8}, doff::Integer, src::Object, soff::Integer, n::Integer)
+function getRange(src::Object, soff::Integer, n::Integer)
     headers = HTTP.Headers()
     HTTP.setheader(headers, contentRange((soff - 1):(soff + n - 2)))
     url = makeURL(src.store, src.key)
-    # avoid extra copy here by passing dest to be written to directly
-    resp = getObject(src.store, url, headers; credentials=src.credentials)
-    copyto!(dest, doff, resp.body)
+    return getObject(src.store, url, headers; credentials=src.credentials).body
+end
+
+function Base.unsafe_copyto!(dest::AbstractVector{UInt8}, doff::Integer, src::Object, soff::Integer, n::Integer)
+    copyto!(dest, doff, getRange(src, soff, n))
+    return n
+end
+
+function Base.unsafe_copyto!(dest::Channel, doff::Integer, src::Object, soff::Integer, n::Integer)
+    bytes = getRange(src, soff, n)
+    dest_arr, new_n = take!(dest)
+    new_n == n || throw(ArgumentError("prefetched number of bytes (`$n`) doesn't matched requested number of bytes (`$new_n`)"))
+    copyto!(dest_arr::AbstractVector{UInt8}, doff, bytes)
+    put!(dest, n)
     return n
 end
 
 mutable struct IOObject{T <: Object} <: IO
     object::T
     pos::Int
+    prefetch::Union{Nothing, Task}
+    chan::Channel{Any}
 end
 
-IOObject(x::Object) = IOObject(x, 1)
+IOObject(x::Object) = IOObject(x, 1, nothing, Channel{Any}(0))
 IOObject(store::AbstractStore, key::String; credentials::Union{CloudCredentials, Nothing}=nothing) =
     IOObject(Object(store, key; credentials))
 
@@ -60,7 +73,17 @@ function Base.readbytes!(x::IOObject, dest::AbstractVector{UInt8}, n::Integer=le
     n = min(n, length(dest))
     n = min(n, length(x.object) - x.pos + 1)
     n == 0 && return dest
-    Base.unsafe_copyto!(dest, 1, x.object, x.pos, n)
-    x.pos += n
+    if x.prefetch === nothing
+        # no prefetch, request directly
+        Base.unsafe_copyto!(dest, 1, x.object, x.pos, n)
+        x.pos += n
+    else
+        put!(x.chan, (dest, n))
+        x.pos += take!(x.chan)::Integer
+    end
+    if !eof(x)
+        # start prefetch
+        x.prefetch = @async Base.unsafe_copyto!(x.chan, 1, x.object, x.pos, n)
+    end
     return dest
 end
