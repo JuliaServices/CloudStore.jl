1df7a592d01f17eef07645f31133cc2bedb28e79
Jacob Quinn <quinn.jacobd@gmail.com>
2023-01-11 16:39:24 -0700

    Account for http redirect responses when downloading
    Fixes https://github.com/JuliaServices/CloudStore.jl/issues/26.
    The issue is that HTTP.jl doesn't consider redirect responses as
    errors by default (they're usually handled automatically via default
    redirect logic, except for 301 statuses, which means the object has
    moved permanently and no redirect location is known).
    
    This PR checks for this case and throw an appropriate ArgumentError.

--- src/get.jl
+++ src/get.jl
@@ -55,6 +55,13 @@ function getObjectImpl(x::AbstractStore, key::String, out::ResponseBodyType=noth
         # make a head request to see if the object happens to be empty
         # if so, it isn't valid to make a Range bytes request, so we'll short-circuit
         resp = API.headObject(x, url, headers; kw...)
+        if HTTP.isredirect(resp)
+            try
+                throw(HTTP.StatusError(resp.status, resp.request.method, resp.request.target, resp))
+            catch
+                throw(ArgumentError("Invalid object key: $key"))
+            end
+        end
         if HTTP.header(resp, "Content-Length") == "0"
             if out === nothing
                 return resp.body
