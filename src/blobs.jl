338f410dfa5be7ca0044e7d9a33fd3fe554220ab
Jacob Quinn <quinn.jacobd@gmail.com>
2023-04-13 22:38:04 -0600

    Add ability to log upload rates (#39)

--- src/blobs.jl
+++ src/blobs.jl
@@ -46,8 +46,8 @@ API.startMultipartUpload(x::Container, key; kw...) = nothing
 
 function API.uploadPart(x::Container, url, part, partNumber, uploadId; kw...)
     blockid = base64encode(lpad(partNumber - 1, 64, '0'))
-    Azure.put(url, [], part; query=Dict("comp" => "block", "blockid" => blockid), kw...)
-    return blockid
+    resp = Azure.put(url, [], part; query=Dict("comp" => "block", "blockid" => blockid), kw...)
+    return (blockid, Base.get(resp.request.context, :nbytes_written, 0))
 end
 
 function API.completeMultipartUpload(x::Container, url, eTags, uploadId; kw...)
