4aa0d163336c5bebcddfe397b9cdb4ac8d607711
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2023-06-02 13:58:41 +0200

    More defensive regex

--- src/parse.jl
+++ src/parse.jl
@@ -84,7 +84,7 @@ end
 function _validate_azure(ok::Bool, host, account, container, blob)
     return (
         ok,
-        isnothing(host) ? nothing : replace(String(host), r"azure"i => "https"; count=1),
+        isnothing(host) ? nothing : replace(String(host), r"^azure"i => "https"; count=1),
         String(validate_account_name(account)),
         isnothing(container) ? "" : String(validate_container_name(container)),
         isnothing(blob) ? "" : String(validate_blob(blob)),
@@ -95,7 +95,7 @@ function _validate_aws(ok::Bool, accelerate, host, bucket, region, key)
     return (
         ok,
         accelerate,
-        isnothing(host) ? nothing : replace(String(host), r"(s|S)3" => "http"; count=1),
+        isnothing(host) ? nothing : replace(String(host), r"^(s|S)3" => "http"; count=1),
         String(validate_bucket_name(bucket, accelerate)),
         isnothing(region) || isempty(region) ? "" : String(validate_region(region)),
         isnothing(key) ? "" : String(validate_key(key)),
